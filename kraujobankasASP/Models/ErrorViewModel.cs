d38009affbd5844e3a6a9214a5d44ca90b23eeb2
Martynas A <11marty11@gmail.com>
2020-03-19 13:57:16 +0200

    initial template

---
+++ kraujobankasASP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace kraujobankasASP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
