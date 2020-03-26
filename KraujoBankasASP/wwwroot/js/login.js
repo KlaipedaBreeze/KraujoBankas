af9c6a9add920ca5e3892bd053466993612f4229
Paulius <p.kasiulynas@gmail.com>
2020-03-26 23:08:57 +0200

    Fixed login and register width problem.

--- KraujoBankasASP/wwwroot/js/login.js
+++ KraujoBankasASP/wwwroot/js/login.js
@@ -1,10 +1,1 @@
-﻿var loginWindow = document.getElementById("login");
-var registerWindow = document.getElementsByClassName("form-control");
-
-//overwriting BS4 conflicting style between login and registration window
-loginWindow.addEventListener("click", () => {
-
-    for (var i = 0; i < registerWindow.length; i++) {
-        registerWindow[i].style.width = "100%";
-    }
-});
+﻿
