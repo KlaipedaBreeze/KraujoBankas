ddb8daf9b0d129bc874a7c5f404f7082e9477885
martynas <11marty11@gmail.com>
2020-03-29 21:28:19 +0300

    added "apie mus" and "Istorija"

--- KraujoBankasASP/wwwroot/js/login.js
+++ KraujoBankasASP/wwwroot/js/login.js
@@ -8,3 +8,12 @@ loginWindow.addEventListener("click", () => {
         registerWindow[i].style.width = "100%";
     }
 });
+
+// When the DOM is ready, run this function
+$(document).ready(function () {
+    //Set the carousel options
+    $('#quote-carousel').carousel({
+        pause: true,
+        interval: 4000,
+    });
+});
